6d5a350419bf336ff7999fe78632ce66776a4d4f
Brett <brett.israelsen@gmail.com>
2014-10-27 10:38:48 -0600

    Brett - Created function and class finder, also a full header parser

--- CUAutogradingScripts/GradeUtils.py
+++ CUAutogradingScripts/GradeUtils.py
@@ -1,7 +1,21 @@
 import re
-from pypeg2 import * #parser
-import pypeg2
+#from pypeg2 import * #parser
+#import pypeg2
+#from antlr4 import * #possibly use this for python3 parsing
 import CppHeaderParser #sudo pip3 install cppheaderparser, also: sudo pip3 install install ply
+
+def funCppHeaderParser(cppSourceFileName):
+    with open (cppSourceFileName, "r") as myfile:
+        data=myfile.read()
+        
+    try:
+        cppHeader = CppHeaderParser.CppHeader(cppSourceFileName)
+        
+    except CppHeaderParser.CppParseError as e:
+        print(e)
+        exit(1)
+        
+    return cppHeader
 
 def getAllNumbersFromString(stringToParse):
     listOfNumbersAsStrings = re.findall("([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)",stringToParse)
@@ -18,22 +32,33 @@ def getAllNumbersFromString(stringToParse):
             None
     return listOfNumbers
     
-def CppFunctionFinder (cppSourceFileName):
-    with open (cppSourceFileName, "r") as myfile:
-        data=myfile.read()
-        
-    try:
-        cppHeader = CppHeaderParser.CppHeader(cppSourceFileName)
-    except CppHeaderParser.CppParseError as e:
-        print(e)
-        sys.exit(1)
+def CppFunctionFinder (cppSourceFileName, full = False):
+    cppHeader = funCppHeaderParser(cppSourceFileName)
 
-    numFunctions = len(cppHeader.functions)
-    print('Number of functions :', numFunctions)
-    
     nameFunctions = []
     for i in range(0, len(cppHeader.functions)):
         nameFunctions.append(cppHeader.functions[i]['name'])
     
-    print('The names of the functions: ', nameFunctions) 
+    if not full:
+        return nameFunctions
+    else:
+        return cppHeader.functions
 
+def cppClassFinder(cppSourceFileName,full = False):
+    cppHeader = funCppHeaderParser(cppSourceFileName)
+
+    nameClasses = []
+    for i in cppHeader.classes.keys():
+        nameClasses.append(cppHeader.classes[i]['name'])
+    
+    if not full:
+        return nameClasses
+    else:
+        return cppHeader.classes
+
+def cppParser(cppSourceFileName):
+    cppHeader = funCppHeaderParser(cppSourceFileName)
+    
+    return cppHeader
+    
+    
--- CUAutogradingScripts/GradeUtilsTest.py
+++ CUAutogradingScripts/GradeUtilsTest.py
@@ -2,9 +2,20 @@ import GradeUtils
 
 def TestGetAllNumbersFromString():
     
-    #GradeUtils.CppFunctionFinder('test.cpp')
-    GradeUtils.CppFunctionFinder('Lab9.cpp')
+    fns = GradeUtils.CppFunctionFinder('test.cpp',full=True)
+    cls  =GradeUtils.cppClassFinder('test.cpp')
+    #fns = GradeUtils.CppFunctionFinder('Lab9.cpp',full=True)
+    #cls = GradeUtils.cppClassFinder('Lab9.cpp',full=True)
     
+    if len(fns) > 1:
+        print('You used too many functions')
+    else:
+        print('Correct number of functions')
+    
+    if len(cls) > 0:
+        print('You were not supposed to use classes')
+    else:
+        print('No classes were used')
     
 def RunAllTests():
     TestGetAllNumbersFromString()
