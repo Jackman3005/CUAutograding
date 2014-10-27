0ef8b613085893ee199252a9eca9f3923a80fd48
Brett <brett.israelsen@gmail.com>
2014-10-27 12:17:23 -0600

    Brett - attempting to use pygccxml

--- CUAutogradingScripts/GradeUtils.py
+++ CUAutogradingScripts/GradeUtils.py
@@ -3,6 +3,27 @@ import re
 #import pypeg2
 #from antlr4 import * #possibly use this for python3 parsing
 import CppHeaderParser #sudo pip3 install cppheaderparser, also: sudo pip3 install install ply
+import os
+import sys
+
+# Find out the file location within the sources tree
+this_module_dir_path = os.path.abspath(
+    os.path.dirname(sys.modules[__name__].__file__))
+# Find out gccxml location
+gccxml_09_path = os.path.join(
+    this_module_dir_path, '..', '..', '..',
+    'gccxml_bin', 'v09', sys.platform, 'bin')
+# Add pygccxml package to Python path
+sys.path.append(os.path.join(this_module_dir_path, '..', '..'))
+
+from pygccxml import parser #sudo pip3 install pygccxml and sudo apt-get install gccxml
+from pygccxml import declarations
+
+# Configure GCC-XML parser
+config = parser.gccxml_configuration_t(
+    gccxml_path=gccxml_09_path,working_directory= this_module_dir_path,cflags = '-std=c++11 -Wall', compiler='g++',ignore_gccxml_output = True)
+
+decls = parser.parse([this_module_dir_path + '/example.hpp'])
 
 def funCppHeaderParser(cppSourceFileName):
     with open (cppSourceFileName, "r") as myfile:
